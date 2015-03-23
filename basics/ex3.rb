0ebbbf0f5b945245222bce875fa60994b34ede01
Calvin Chu <calvinchu8172@gmail.com>
2015-03-23 15:11:12 +0800

    basics ex3

---
+++ basics/ex3.rb
@@ -0,0 +1,12 @@
+movies = {
+  alien: 1975,
+  the_day_after_tomorrow: 2004,
+  the_avangers: 2013,
+  matrix: 2001,
+  back_to_the_future: 1981
+}
+puts movies[:alien]
+puts movies[:the_day_after_tomorrow]
+puts movies[:the_avangers]
+puts movies[:matrix]
+puts movies[:back_to_the_future]
